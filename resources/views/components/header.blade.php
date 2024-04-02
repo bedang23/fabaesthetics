15a03b167b087808243d22f4bd2687be88798ee4
bedang23 <bedangbanerjee@gmail.com>
2024-04-03 00:30:02 +0530

    initial view setup

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,46 @@
+		<!-- Header
+		============================================= -->
+		<header id="header" class="transparent-header floating-header dark custom-header-size" data-sticky-shrink="false">
+			<div id="header-wrap" class="border-bottom-0">
+				<div class="container pe-md-0">
+					<div class="header-row">
+
+						<!-- Logo
+						============================================= -->
+						<div id="logo" class="me-lg-4 pe-lg-4">
+							<a href="index.html">
+								<img class="logo-default" srcset="assets/images/logo.png, assets/images/logo@2x.png 2x" src="assets/images/logo@2x.png" alt="Canvas Logo">
+							</a>
+						</div><!-- #logo end -->
+
+						<div class="primary-menu-trigger">
+							<button class="cnvs-hamburger" type="button" title="Open Mobile Menu">
+								<span class="cnvs-hamburger-box"><span class="cnvs-hamburger-inner"></span></span>
+							</button>
+						</div>
+
+						<div class="header-misc ms-lg-auto">
+							<a href="#" class="fw-medium font-primary" data-scrollto="#section-appointment" data-easing="easeInOutExpo" data-speed="1250" data-offset="140">Book Now</a>
+						</div>
+
+						<!-- Primary Navigation
+						============================================= -->
+						<nav class="primary-menu">
+
+							<ul class="menu-container style-2 one-page-menu">
+								<li class="menu-item border-light current"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="0" data-href="#slider"><div class="overflow-hidden"><div data-animate="fadeInDownSmall">Home</div></div></a></li>
+								<li class="menu-item border-light"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="130" data-href="#section-about"><div class="overflow-hidden"><div data-animate="fadeInDownSmall" data-delay="200">About</div></div></a></li>
+								<li class="menu-item border-light"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="130" data-href="#section-services"><div class="overflow-hidden"><div data-animate="fadeInDownSmall" data-delay="200">Services</div></div></a></li>
+								<li class="menu-item border-light"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="0" data-href="#section-price"><div class="overflow-hidden"><div data-animate="fadeInDownSmall" data-delay="400">Price</div></div></a></li>
+								<li class="menu-item border-light"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="130" data-href="#section-testimonials"><div class="overflow-hidden"><div data-animate="fadeInDownSmall" data-delay="600">Testimonials</div></div></a></li>
+								<li class="menu-item border-light"><a class="menu-link" href="#" data-easing="easeInOutExpo" data-speed="1250" data-offset="130" data-href="#section-shop"><div class="overflow-hidden"><div data-animate="fadeInDownSmall" data-delay="800">Shop</div></div></a></li>
+							</ul>
+
+						</nav>
+
+					</div>
+				</div>
+			</div>
+			<div class="header-wrap-clone"></div>
+		</header>
+		<!-- #header end -->
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+Fabauthestic Home Page
+@endsection('content')
